2be86f4a74ade964c7fabd9ce269800790fbba56
Luiz Gustavo <gusstavopdm@gmail.com>
2022-12-16 10:11:17 -0300

    feat(prisma): adicionando PrismaNotificationMapper

---
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -0,0 +1,16 @@
+import { Notification } from '@app/entities/notification';
+
+class PrismaNotificationMapper {
+  static toPrisma(notification: Notification) {
+    return {
+      id: notification.id,
+      category: notification.category,
+      content: notification.content.value,
+      recipientId: notification.recipientId,
+      readAt: notification.readAt,
+      createdAt: notification.createdAt,
+    };
+  }
+}
+
+export { PrismaNotificationMapper };
