3a4f6856ed63ff7f38b98634c248b5d33662f858
Luiz Gustavo <gusstavopdm@gmail.com>
2022-12-16 11:38:59 -0300

    feat(HttpModule): adicionando import services

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,23 @@
+import { CountRecipientNotifications } from '@app/use-cases/count-recipient-notifications';
 import { SendNotification } from '@app/use-cases/send-notification';
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notifications.controller';
 import { DatabaseModule } from '../database/database.module';
+import { CancelNotification } from '@app/use-cases/cancel-notification';
+import { ReadNotification } from '@app/use-cases/read-notification';
+import { UnreadNotification } from '@app/use-cases/unread-notification';
+import { GetRecipientNotifications } from '@app/use-cases/get-recipient-notifications';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    ReadNotification,
+    UnreadNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+  ],
 })
 export class HttpModule {}
