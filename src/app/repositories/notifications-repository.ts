b88e5a153d680114810ab3051f154673ddea0de0
Luiz Gustavo <gusstavopdm@gmail.com>
2022-12-16 10:02:43 -0300

    feat(NotificationsRepository): adicionando save

--- src/app/repositories/notifications-repository.ts
+++ src/app/repositories/notifications-repository.ts
@@ -3,6 +3,7 @@ import { Notification } from '@app/entities/notification';
 abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>;
   abstract findById(notificationId: string): Promise<Notification | null>;
+  abstract save(notification: Notification): Promise<void>;
 }
 
 export { NotificationsRepository };
