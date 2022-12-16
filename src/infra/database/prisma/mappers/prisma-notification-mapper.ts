5af285c536ba9e783932f9eeef720ee33fbb507d
Luiz Gustavo <gusstavopdm@gmail.com>
2022-12-16 11:59:48 -0300

    feat(PrismaNotificationMapper): adicionando canceledAt no toPrisma

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -10,6 +10,7 @@ class PrismaNotificationMapper {
       content: notification.content.value,
       recipientId: notification.recipientId,
       readAt: notification.readAt,
+      canceledAt: notification.canceledAt,
       createdAt: notification.createdAt,
     };
   }
